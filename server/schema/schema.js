ed50e9098d048ac1d95c3ccec56200efaf47840c
Tyra Ferguson <tyraferguson@Tyras-Mac-mini.local>
2022-10-15 18:16:16 -0400

    server development_1

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,33 +1,77 @@
 const {projects, clients} = require('../sampleData.js')
 
-const { GraphQLObjectType, GraphQLID, GraphQLString, GraphQLSchema} = require('graphql')
+const { GraphQLObjectType, GraphQLID, GraphQLString, GraphQLSchema, GraphQLList} = require('graphql')
 
 //Make type for each objects
 //Client Type
 const ClientType = new GraphQLObjectType({
     name: 'Client',
+    //Query fields which returns objects
     fields: () => ({
         id: {type: GraphQLID}, 
         name: {type: GraphQLString},
         email: {type: GraphQLString}, 
         phone: {type: GraphQLString}
-    })
-})
-//To make a query we need to make a root query object
+    }),
+});
+
+//Project Type
+const ProjectType = new GraphQLObjectType({
+    name: 'Project',
+    //Query fields which returns objects
+    fields: () => ({
+        id: {type: GraphQLID}, 
+        name: {type: GraphQLString},
+        description: {type: GraphQLString}, 
+        status: {type: GraphQLString},
+        client: {
+            type: ClientType,
+            resolve(parent, args) {
+                return clients.find((client) => client.id === parent.clientId);
+            }
+        }
+    }),
+});
+//To make a query by ID we need to make a root query object
 // Root Query
 const RootQuery = new GraphQLObjectType({
     name: 'RootQueryType',
-    //Query fields
     fields: {
+        //get all clients
+        clients:{
+            type: new GraphQLList(ClientType),
+            resolve(parent, args) {
+                return clients;
+            },
+        },
+        //return client based on ID
         client: {
             type: ClientType,
             args: {id:{type: GraphQLID}},
             resolve(parent, args) {
-                return clients.find(client => client.id === args.id)
-            }
-        }
-    }
-})
+                return clients.find(client => client.id === args.id);
+            },
+        },
+        //return all projects
+        projects:{
+            type: new GraphQLList(ProjectType),
+            resolve(parent, args) {
+                return projects;
+            },
+        },
+        //return project based on ID
+        project: {
+            type: ProjectType,
+            args: {id:{type: GraphQLID}},
+            resolve(parent, args) {
+                return projects.find((project) => project.id === args.id);
+            },
+        },
+    },
+});
+
+
+//To be able to use this we need to export this as a GraphQLSchema
 module.exports = new GraphQLSchema ({
     query:RootQuery
 })
--- server/server.js
+++ server/server.js
@@ -6,17 +6,10 @@ const schema = require('./schema/schema');
 
 var { buildSchema } = require('graphql');
 
-// var schema = buildSchema(`
-//   type Query {
-//     hello: String
-//   }
-// `);
-
-
-
 const app = express();
 app.use('/graphql', graphqlHTTP({
   schema,
+  //for development, so set to true if in development
   graphiql: process.env.NODE_ENV === 'development',
 }));
 app.listen(port, () => console.log('Now browse to localhost:4000/graphql'));
